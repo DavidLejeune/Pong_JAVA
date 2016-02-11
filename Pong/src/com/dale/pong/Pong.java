aab98e498c91e8ba09a4be9aeefbaee434fd84fb
DavidLejeune <Lejeune.David@outlook.com>
2016-02-11 17:00:32 +0100

    Renderer class + frame created

--- Pong/src/com/dale/pong/Pong.java
+++ Pong/src/com/dale/pong/Pong.java
@@ -5,6 +5,9 @@
  */
 package com.dale.pong;
 
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
 /**
  *
  * @author David
@@ -13,15 +16,29 @@ public class Pong
 {
     
     public static Pong pong;
-
+    public int width = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
+    public int height = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height - 50;
+    public Renderer renderer;
+    
     public Pong() 
     {
+        JFrame jframe = new JFrame("Pong");
+        renderer = new Renderer();
+        
+        jframe.setSize(width,height);
+        jframe.setVisible(true);
+        jframe.add(renderer);
         
     }
 
-
+    public void render(Graphics g) 
+    {
+        
+    }
+    
     public static void main(String[] args) 
     {
         pong = new Pong();
     }
+
 }
---
+++ Pong/src/com/dale/pong/Renderer.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dale.pong;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+
+/**
+ *
+ * @author David
+ */
+public class Renderer extends JPanel
+{
+
+    private static final long serialVersionUID = 1L;
+
+    
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
+        
+        Pong.pong.render(g);
+        
+    }
+    
+    
+    
+    public Renderer() 
+    {
+        
+    }
+    
+    
+    
+}
